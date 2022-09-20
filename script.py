74705d82d68babc113834562e185fa725c93a236
SulemanHamdani <sulemanlad8@gmail.com>
2022-09-20 11:54:01 +0500

    Init

---
+++ script.py
@@ -0,0 +1,12 @@
+import os, sys
+import PIL
+from PIL import Image
+
+for infile in sys.argv[1:]:
+    f, e = os.path.splitext(infile)
+    outfile = f + ".jpg"
+    if infile != outfile:
+        try:
+            Image.open(infile).save(outfile)
+        except IOError:
+            print ("cannot convert", infile)
